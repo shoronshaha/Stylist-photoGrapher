ecc5718b53c225254ffddb67130c946355e61c4c
shoronshaha <smaranshaha@gmail.com>
2022-04-17 03:59:23 +0600

    added header and notfound page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Shared/Header/Header';
+import NotFound from './Components/Shared/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-5xl'>hello</h1>
+      <Header></Header>
+      <Routes>
+        {/*  <Route path='/' element={<Home />} />
+        <Route path='/reviews' element={<Reviews />} />
+        <Route path='/checkout' element={<Checkout />} />
+        <Route path='/blogs' element={<Blogs />} />
+        <Route path='/about' element={<About />} /> */}
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Shared/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
+
+const Header = () => {
+    return (
+        <div className='w-full top-0 bg-purple-800 px-12 py-4 flex justify-center md:justify-between'>
+            <div className='hidden md:block'>
+                <Link className='font-sans font-bold text-3xl text-white' to='/'>
+                    Stylist PhotoGrapher
+                </Link>
+            </div>
+            <div className='flex font-black gap-2 text-[16px]'>
+                <CustomLink to='/'>HOME</CustomLink>
+                <CustomLink to='/reviews'>REVIEWS</CustomLink>
+                <CustomLink to='/dashboard'>CHECKOUT</CustomLink>
+                <CustomLink to='/blogs'>BLOGS</CustomLink>
+                <CustomLink to='/about'>ABOUT</CustomLink>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
