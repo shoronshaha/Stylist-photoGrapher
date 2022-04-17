c69fb87370b547379db1baef310b9802b0e5c4cc
shoronshaha <smaranshaha@gmail.com>
2022-04-18 03:37:38 +0600

    added google authentication

--- src/Components/Login/SocialLogin/SocialLogin.js
+++ src/Components/Login/SocialLogin/SocialLogin.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../../Firebase/Firebase.init';
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+
+    let errorElement;
+
+    if (error) {
+        errorElement = <div>
+            <p className='text-red-600'>Error: {error.message}</p>
+        </div>
+    }
+
+
+    if (user) {
+        navigate('/home');
+    }
+
     return (
         <div>
             <div className='flex justify-center my-4 '>
@@ -8,10 +27,11 @@ const SocialLogin = () => {
                 <p className='px-2'>or</p>
                 <div style={{ height: "2px" }} className='bg-blue-700 w-[100px] mt-3'></div>
             </div>
+            {errorElement}
             <div>
-                <button value="button" className="px-4 py-2 rounded bg-green-500 text-white hover:bg-green-700 my-4 w-full" id="whoobe-ibemp">Google Sign In</button>
-                <button value="button" className="px-4 py-2 rounded bg-indigo-400 text-white hover:bg-indigo-700 my-4 w-full" id="whoobe-ibemp">Github Sign In</button>
-                <button value="button" className="px-4 py-2 rounded bg-blue-400 text-white hover:bg-blue-700 my-4 w-full" id="whoobe-ibemp">Facebook Sign In</button>
+                <button value="button" onClick={() => signInWithGoogle()} className="px-4 py-2 rounded-full bg-green-500 text-white hover:bg-green-700 my-4 w-full" > Google Sign In</button>
+                <button value="button" className="px-4 py-2 rounded-full bg-indigo-400 text-white hover:bg-indigo-700 my-4 w-full" > Github Sign In</button>
+                <button value="button" className="px-4 py-2 rounded-full bg-blue-400 text-white hover:bg-blue-700 my-4 w-full "> Facebook Sign In</button>
             </div>
         </div>
     );
