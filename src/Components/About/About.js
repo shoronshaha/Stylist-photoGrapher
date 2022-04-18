2bb896b5dab2ede8e3aba15ab5c126383e7eff3e
shoronshaha <smaranshaha@gmail.com>
2022-04-18 17:50:04 +0600

    added some style modify

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,7 +8,9 @@ const About = () => {
                 <div className='m-10'>
                     <h1 className='text-6xl text-orange-500 font-bold'>Shoron <span className='text-blue-400'>Shaha</span></h1>
                     <p className='mt-5 text-[30px] text-purple-500 font-bold'>Junior Developer</p>
+                    <p className='font-bold'>My objective is to obtain a challenging position in a high quality engineering environment where my resourceful experience and academic skills will add value to organizational operations.</p>
                 </div>
+
                 <div className=''>
                     <img className='w-[300px] rounded-full' src={profile} alt="" />
                 </div>
--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import Thumbnail from '../../../Images/Thumbnail/photographer 2.jpg'
+import Thumbnail from '../../../Images/Thumbnail/photographer 2.jpg';
+import './Banner.css';
 
 const Banner = () => {
     return (
-        <div>
+        <div className='containers'>
             <img style={{ width: '100%' }} src={Thumbnail} alt="" />
+
+            <div>
+                <h1 className='centers text-5xl font-bold text-white-500'>Stylist Photographer</h1>
+            </div>
         </div>
     );
 };
